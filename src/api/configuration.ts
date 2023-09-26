f0fd33e1bf54d52e00af659e512a5e50361e6c7a
“Achen” <wgjiachen@163.com>
2023-09-26 20:37:04 +0800

    #design 引入日志中间件

--- src/api/configuration.ts
+++ src/api/configuration.ts
@@ -1,11 +1,13 @@
 import { createConfiguration, hooks } from '@midwayjs/hooks';
 import * as Koa from '@midwayjs/koa';
 import * as jwt from '@midwayjs/jwt';
+import logger from './middle/logger'
 /**
  * setup midway server
  */
 export default createConfiguration({
   imports: [Koa, jwt, hooks({
+    middleware: [logger]
   })],
   importConfigs: [{ 
     default: { keys: 'session_keys' },
