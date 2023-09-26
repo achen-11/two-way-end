b6f64a24cab53f865c0cbf36525fd04ede3d8ab6
“Achen” <wgjiachen@163.com>
2023-09-26 20:36:34 +0800

    #desgin 引入jwt & md5

--- src/api/configuration.ts
+++ src/api/configuration.ts
@@ -1,10 +1,17 @@
 import { createConfiguration, hooks } from '@midwayjs/hooks';
 import * as Koa from '@midwayjs/koa';
-
+import * as jwt from '@midwayjs/jwt';
 /**
  * setup midway server
  */
 export default createConfiguration({
-  imports: [Koa, hooks()],
-  importConfigs: [{ default: { keys: 'session_keys' } }],
+  imports: [Koa, jwt, hooks({
+  })],
+  importConfigs: [{ 
+    default: { keys: 'session_keys' },
+    jwt: {
+      secret: process.env.JWT_KEY, // fs.readFileSync('xxxxx.key')
+      expiresIn: '7d', // https://github.com/vercel/ms
+    }
+  }],
 });
