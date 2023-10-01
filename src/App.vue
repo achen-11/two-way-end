e4ef5209e3d4b04110136283abfe094c7742a46f
“Achen” <wgjiachen@163.com>
2023-10-01 21:40:32 +0800

    #feat antd-全局中文

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="app">
-    <router-view></router-view>
-   </div>
+  <a-config-provider :locale="locale">
+    <div class="app">
+      <router-view></router-view>
+    </div>
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
 
+
+const locale = zhCN
 
 </script>
 
