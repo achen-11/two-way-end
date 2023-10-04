f9ff939dfbd3411b78355cb15d5612924dc842bd
“Achen” <wgjiachen@163.com>
2023-10-04 12:18:21 +0800

    #feat 引入redis

--- src/api/configuration.ts
+++ src/api/configuration.ts
@@ -1,19 +1,26 @@
 import { createConfiguration, hooks } from '@midwayjs/hooks';
 import * as Koa from '@midwayjs/koa';
 import * as jwt from '@midwayjs/jwt';
+import * as redis from '@midwayjs/redis';
 import logger from './middle/logger'
 /**
  * setup midway server
  */
 export default createConfiguration({
-  imports: [Koa, jwt, hooks({
+  imports: [Koa, jwt, redis, hooks({
     middleware: [logger]
   })],
   importConfigs: [{ 
-    default: { keys: 'session_keys' },
-    jwt: {
-      secret: process.env.JWT_KEY, // fs.readFileSync('xxxxx.key')
-      expiresIn: '7d', // https://github.com/vercel/ms
-    }
+    default: { 
+      keys: 'session_keys',
+      redis: {
+        client: {
+          port: 6379, // Redis port
+          host: "127.0.0.1", // Redis host
+          password: "123123",
+          db: 0,
+        },
+      },
+     },
   }],
 });
--- src/api/test.ts
+++ src/api/test.ts
@@ -4,14 +4,15 @@ import { JwtService } from '@midwayjs/jwt';
 import md5 from 'md5'
 import {failRsp, successRsp} from './utils'
 import { jwtMiddleWare } from './middle/jwt';
+import { RedisService } from '@midwayjs/redis';
 
 export const login = Api(
   Get('/test'),
-  Middleware(jwtMiddleWare),
   async () => {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcl9pZCI6ImFkbWluIiwibmFtZSI6Iui2hee6p-euoeeQhuWRmCIsImNyZWF0ZWRUaW1lIjoiMjAyMy0wOS0yNlQwOTo1Nzo1Mi4wMDBaIiwidXBkYXRlZFRpbWUiOiIyMDIzLTA5LTI2VDA5OjU3OjU0LjAwMFoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2OTU3MzIwODAsImV4cCI6MTY5NjMzNjg4MH0.bg1zDNIPlueaDZwSiVMBAozXs_aby5dMlUUo57D4Ueg'
-    const jwt = await useInject(JwtService)
-    const res = jwt.decode(token)
-    return successRsp(res)
+
+    const redis = await useInject(RedisService)
+    await redis.set('token', token)
+    return successRsp(token)
   }
 )
