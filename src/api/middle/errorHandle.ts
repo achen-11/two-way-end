21212bd993466cb6eb8318750a63471d628e7c3c
“Achen” <wgjiachen@163.com>
2023-11-20 16:40:40 +0800

    #perf 优化异常处理->写入日志

--- src/api/middle/errorHandle.ts
+++ src/api/middle/errorHandle.ts
@@ -1,12 +1,26 @@
 import { Context } from '@midwayjs/koa';
 import { useContext, useInject } from '@midwayjs/hooks';
 import { failRsp } from '../utils/utils'
+import dayjs from 'dayjs';
+import fs from 'fs'
 
 export default async (next: any) => {
   const ctx = useContext<Context>();
   try {
     await next()
   } catch (error) {
+    const path = `./logs/request-error/${dayjs().format('YYYY-MM-DD')}.log`
+    const errorContent = 
+`
+${dayjs().format('YYYY-MM-DD HH:mm:ss')}
+================ [${ctx.method}] ${ctx.url} ================
+${error.message}
+=================${'='.repeat(ctx.method.length + ctx.url.length + 3)}=================
+`
+    fs.writeFile(path, errorContent, { flag: 'a' }, (e)=>{
+      console.log('write-error', error?.message);
+      
+    })
     return failRsp(error.message, 500, null)
   }
 }
