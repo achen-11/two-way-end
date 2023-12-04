c27a34804cde8ff935d2be5a720cc0eec0f65af6
“Achen” <wgjiachen@163.com>
2023-12-04 13:44:08 +0800

    #bug 修复新增弹窗通知-跳转错误

--- src/view/announce/modal.vue
+++ src/view/announce/modal.vue
@@ -42,7 +42,7 @@ const param = 'announce'
 // 跳转到创建通知
 const router = useRouter()
 const handleCreate = () => {
-  router.push('/announce/detail?id=-1')
+  router.push('/announce/edit?id=-1')
 }
 
 /**Table */
